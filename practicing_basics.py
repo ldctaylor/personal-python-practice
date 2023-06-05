14e836a0d6334b041fcdc41a562e31e928c8175d
Leah <ldctaylor@gmail.com>
2023-06-05 14:24:02 +1000

    Finished the code, removed redundant case

--- practicing_basics.py
+++ practicing_basics.py
@@ -1,7 +1,7 @@
 #I'm just practicing basic skills learnt. I've put various beginner101 practices here but put them together so that the user is prompted to specify which one he wants to run at the command line.
 
 #defining my exercise list as a global variable.
-exerciselist = ["emojis", "service charge", "quieten down", "douglas adams"]
+exerciselist = ["emojis", "service charge", "quieten down"]
 
 def main():
     #Prompt the user to specify the exercise they'd like demonstrated. My ability to validate input is still pretty limited but I have stripped leading or trailing whitespace and forced lowercase so that the input is treated case insensitively.
@@ -14,8 +14,6 @@ def main():
             calculate_service()
         case "quieten down":
             quieten_or_slow_down()
-        case "douglas adams":
-            pass #STILL TO COME
         case _:
             print("Sorry, I did not understand your input. I'm spontaneously combusting.")
 
