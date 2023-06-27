ad7d361be86e49208eb2fb45d0157545a939c33f
Leah <ldctaylor@gmail.com>
2023-06-27 07:55:56 +0100

    updating regochecker

--- regochecker.py
+++ regochecker.py
@@ -1,24 +1,32 @@
+# Implement a program that prompts the user for a rego plate and outputs valid if meets all of the requirements or invalid if it does not. 
+# Assume any letters in the user’s input will be uppercase. 
+# Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. 
+# “All plates must start with at least two letters.”
+# Plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+# Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+# “No periods, spaces, or punctuation marks are allowed.”
+
 def main():
-    s = input("Plate: ")
+    plate = input("Plate: ")
     #Debugging:
     # print(f"is_two_letters is{is_two_letters(s)}")
     # print(f"are_numbers_at_end is {are_numbers_at_end(s)}")
     # print(f"is_chars_count is {is_chars_count(s)}")
     # print(f"valid_chars is {valid_chars(s)}")
     # print(f"is_valid is {is_valid(s)}")
-    if is_valid(s):
+    if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
-def is_valid(s):
-    if not is_two_letters(s):
+def is_valid(plate):
+    if not is_two_letters(plate):
         return False
-    if not are_numbers_at_end(s):
+    if not are_numbers_at_end(plate):
         return False
-    if not is_chars_count(s):
+    if not is_chars_count(plate):
         return False
-    if not valid_chars(s):
+    if not valid_chars(plate):
         return False
     return True
 
@@ -28,21 +36,21 @@ def is_chars_count(s):
 def is_two_letters(s):
     return s[0:2].isalpha()
 
-def are_numbers_at_end(plate):
+def are_numbers_at_end(s):
     if plate.isalpha():
         return True
-    for index in range(len(plate)-1):
+    for index in range(len(s)-1):
         if plate[index].isalpha():
             if plate[index+1] == "0": #checks first digit is not 0
                 return False
         else:
-            for char in plate[index+1:len(plate)]: #checks the subsequent character is not alpha
+            for char in s[index+1:len(s)]: #checks the subsequent character is not alpha
                 if char.isalpha():
                     return False
             return True
 
-def valid_chars(plate):
-    if plate.isalnum() == False:
+def valid_chars(s):
+    if s.isalnum() == False:
         return False
     return True
 
