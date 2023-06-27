caf202c542b53482a560303040f7078a1a0436b9
Leah <ldctaylor@gmail.com>
2023-06-27 07:59:00 +0100

    updating regochecker

--- regochecker.py
+++ regochecker.py
@@ -1,6 +1,6 @@
-# Implement a program that prompts the user for a rego plate and outputs valid if meets all of the requirements or invalid if it does not. 
-# Assume any letters in the user’s input will be uppercase. 
-# Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. 
+# Implement a program that prompts the user for a rego plate and outputs valid if meets all of the requirements or invalid if it does not.
+# Assume any letters in the user’s input will be uppercase.
+# Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not.
 # “All plates must start with at least two letters.”
 # Plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
 # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
@@ -37,11 +37,11 @@ def is_two_letters(s):
     return s[0:2].isalpha()
 
 def are_numbers_at_end(s):
-    if plate.isalpha():
+    if s.isalpha():
         return True
     for index in range(len(s)-1):
-        if plate[index].isalpha():
-            if plate[index+1] == "0": #checks first digit is not 0
+        if s[index].isalpha():
+            if s[index+1] == "0": #checks first digit is not 0
                 return False
         else:
             for char in s[index+1:len(s)]: #checks the subsequent character is not alpha
@@ -55,4 +55,3 @@ def valid_chars(s):
     return True
 
 main()
-
